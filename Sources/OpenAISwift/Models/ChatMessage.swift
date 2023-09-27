23bad31a49b49cc5b6d4965bceaed6c664774bd3
Aron Budinszky <aron@budinszky.me>
2023-09-27 16:56:16 +0200

    Add coding keys to remove invalid id from req body

--- Sources/OpenAISwift/Models/ChatMessage.swift
+++ Sources/OpenAISwift/Models/ChatMessage.swift
@@ -19,7 +19,11 @@ public enum ChatRole: String, Codable {
 
 /// A structure that represents a single message in a chat conversation.
 public struct ChatMessage: Codable, Identifiable {
-    // uuid to conform to Identifiable protocol
+    /// Id is not allowed in message body
+    private enum CodingKeys: String, CodingKey {
+        case role, content
+    }
+    /// uuid to conform to Identifiable protocol
     public var id = UUID()
     /// The role of the sender of the message.
     public let role: ChatRole?
